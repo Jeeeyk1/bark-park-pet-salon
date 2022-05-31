2e5acda381c48b110a91333aa319c15435e05daa
Jeeyk1 <nooojake@yahoo.com>
2022-05-31 19:38:54 +0800

    Fix Error

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,10 +2,8 @@
 
 //import db from "../../utils/db";
 
-export default  async function handler(req, res) {
- // await db.connect();
+export default async function handler(req, res) {
+  // await db.connect();
   //await db.disconnect();
-  res.status(200).json({ name: 'Justin Jake' });
+  res.status(200).json({ name: "Justin Jake" });
 }
-
-
