dbfc9739a8468cb0c30a2cd64d942765b3a8a4bc
Jeeyk1 <nooojake@yahoo.com>
2022-05-31 19:35:28 +0800

    Remove Error

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -22,7 +22,6 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import useStyles from "../../utils/styles";
 import CheckoutWizard from "../../components/CheckoutWizard";
-import { useSnackbar } from "notistack";
 import { getError } from "../../utils/error";
 import axios from "axios";
 
@@ -85,7 +84,7 @@ function Order({ params }) {
       fetchOrder();
     }
   }, [order]);
-  const { closeSnackbar, enqueueSnackbar } = useSnackbar();
+
   return (
     <Layout title={`Order ${orderId}`}>
       <CheckoutWizard activeStep={4}></CheckoutWizard>
