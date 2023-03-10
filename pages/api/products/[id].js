02f4e4e7d05f601e9733fe8e1338a7602c1213ac
Jeeyk1 <nooojake@yahoo.com>
2023-03-10 18:08:11 +0800

    fixing prod

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -7,7 +7,7 @@ const handler = nc();
 handler.get(async (req, res) => {
   await db.connect();
   const product = await Product.findById(req.query.id);
-  await db.disconnect();
+  db.disconnect();
   res.send(product);
 });
 
--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -7,7 +7,7 @@ const handler = nc();
 handler.get(async (req, res) => {
   await db.connect();
   const products = await Product.find({});
-  await db.disconnect();
+  db.disconnect();
   res.send(products);
 });
 
